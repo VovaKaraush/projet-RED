d6858b668b83b2e847a8cc3adea990e882fc5cbd
Nathan <nathan.gueroult.34@gmail.com>
2025-09-15 01:44:33 +0200

    ajout et test de initCaracter et displayInfo

--- src/main.go
+++ src/main.go
@@ -2,7 +2,33 @@ package main
 
 import "fmt"
 
+type Character struct {
+	nom string
+	classe string
+	niveau uint
+	pvMax uint
+	pv int
+	inventaire []string
+}
+
+func initCaracter(nom, classe string, niveau, pvMax uint, pv int, inventaire []string) Character{
+	return Character{
+		nom: nom, 
+		classe: classe, 
+		niveau: niveau, 
+		pvMax: pvMax, 
+		pv: pv, 
+		inventaire: inventaire,
+	}
+}
+func displayInfo(c Character) {
+	fmt.Print("Nom : ", c.nom, "\nClasse : ", c.classe, "\nNiveau : ", c.niveau, "\nVie : ", c.pv, "/", c.pvMax, "\n")
+}
+
 func main() {
-	fmt.Println("test")
-	fmt.Println("test2")
+	var n string
+	fmt.Print("Choisissez un nom : ")
+	fmt.Scanln(&n)
+	c1 := initCaracter(n, "Elfe", 1, 100, 40, []string{"potion", "potion", "potion"})
+	displayInfo(c1)
 }
