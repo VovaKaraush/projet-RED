3415a96562d2d6f8b5762874bb4d75bdc8d8312b
thugo <hugo.tronche@ynove.com>
2025-09-15 10:49:52 +0200

    piscine

--- src/main.go
+++ src/main.go
@@ -6,12 +6,12 @@ type Character struct {
 	nom string
 	classe string
 	niveau uint
-	pvMax uint
+	pvMax int
 	pv int
 	inventaire []string
 }
 
-func initCaracter(nom, classe string, niveau, pvMax uint, pv int, inventaire []string) Character{
+func initCaracter(nom, classe string, niveau uint, pvMax int, pv int, inventaire []string) Character{
 	return Character{
 		nom: nom, 
 		classe: classe, 
@@ -25,10 +25,19 @@ func displayInfo(c Character) {
 	fmt.Print("Nom : ", c.nom, "\nClasse : ", c.classe, "\nNiveau : ", c.niveau, "\nVie : ", c.pv, "/", c.pvMax, "\n")
 }
 
+func isDead(c Character) Character{
+    if c.pv <= 0 {
+        
+		c.pv = c.pvMax / 2
+	}
+	return(c)
+}
+
 func main() {
 	var n string
 	fmt.Print("Choisissez un nom : ")
 	fmt.Scanln(&n)
 	c1 := initCaracter(n, "Elfe", 1, 100, 40, []string{"potion", "potion", "potion"})
+	c1 = isDead(c1)
 	displayInfo(c1)
 }
