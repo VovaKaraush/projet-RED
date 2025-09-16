b28e4577309cba0c446b8f19b27ee1670c91afba
VovaKaraush <starper200765@gmail.com>
2025-09-16 16:55:48 +0200

    rien ne marche

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,3 @@
-module modules
+module main
 
 go 1.25.1
--- src/modules/character.go
+++ src/modules/character.go
@@ -1,8 +1,17 @@
 package modules
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type Character struct {
+	nom        string
+	classe     string
+	niveau     uint
+	pvMax      int
+	pv         int
+	skill      []string
+	inventaire map[string]int
+	argent     int
+}
 
 func inputName() string {
 	var n string
@@ -30,20 +39,34 @@ func initCaracter(nom, classe string, niveau uint, pvMax int, pv int, skill []st
 	}
 }
 
-type Character struct {
-	nom        string
-	classe     string
-	niveau     uint
-	pvMax      int
-	pv         int
-	skill      []string
-	inventaire map[string]int
-	argent     int
-}
-
 func isDead(c Character) Character {
 	if c.pv <= 0 {
 		c.pv = c.pvMax / 2
 	}
 	return (c)
+}
+
+func characterCreation() Character {
+	n := inputName()
+	var c string
+	for c != "1" && c != "2" && c != "3" {
+		fmt.Print("Choisissez une classe parmi :\n1-Humain\n2-Elfe\n3-Nain\n\n")
+		fmt.Scanln(&c)
+		if c != "1" && c != "2" && c != "3" {
+			fmt.Println("Commande inconnue")
+		}
+	}
+	var pvMax int
+	switch c {
+	case "1":
+		c = "Humain"
+		pvMax = 100
+	case "2":
+		c = "Elfe"
+		pvMax = 80
+	case "3":
+		c = "Nain"
+		pvMax = 120
+	}
+	return initCaracter(n, c, 0, pvMax, pvMax/2, []string{"Coup de poing"}, map[string]int{"Potion de vie": 3}, 100)
 }
