d2f65633c45b4cfc880c615994f05a3a224b09dc
VovaKaraush <starper200765@gmail.com>
2025-09-17 09:11:52 +0200

    test reinsertion des changements dans characterCreation

--- src/modules/character.go
+++ src/modules/character.go
@@ -17,7 +17,7 @@ type Character struct {
 	pv         int
 	skill      []string
 	inventaire map[string]Objet
-	argent	   int
+	argent     int
 }
 
 func inputName() string {
@@ -41,16 +41,13 @@ func initCharacter(nom, classe string, niveau uint, pvMax int, pv int, skill []s
 		pvMax:      pvMax,
 		pv:         pv,
 		skill:      skill,
-		inventaire: inventaire, 
-		argent:		argent,
+		inventaire: inventaire,
+		argent:     argent,
 	}
 }
 
-func CharacterCreation() Character{
-	var n string
-	fmt.Print("Choisissez un nom : ")
-	fmt.Scanln(&n)
-	n = capitalizeFirstLetter(n)
+func CharacterCreation() Character {
+	n := inputName()
 	var c string
 	for c != "1" && c != "2" && c != "3" {
 		fmt.Print("Choisissez une classe parmi :\n1-Humain\n2-Elfe\n3-Nain\n\n")
@@ -72,13 +69,13 @@ func CharacterCreation() Character{
 		pvMax = 120
 	}
 	inventaire := map[string]Objet{
-		"Potion de vie": Objet{1, 3, 3, 1}, 
-		"Potion de poison": Objet{2, 0, 6, 1}, 
-		"Livre de sort : Boule de feu": Objet{3, 0, 25, 1}, 
-		"Fourrure de loup": Objet{4, 0, 4, 3}, 
-		"Peau de troll": Objet{5, 0, 3, 7}, 
-		"Cuir de sanglier": Objet{6, 0, 3, 3}, 
-		"Plume de corbeau": Objet{7, 0, 1, 3},
+		"Potion de vie":                Objet{1, 3, 3, 1},
+		"Potion de poison":             Objet{2, 0, 6, 1},
+		"Livre de sort : Boule de feu": Objet{3, 0, 25, 1},
+		"Fourrure de loup":             Objet{4, 0, 4, 3},
+		"Peau de troll":                Objet{5, 0, 3, 7},
+		"Cuir de sanglier":             Objet{6, 0, 3, 3},
+		"Plume de corbeau":             Objet{7, 0, 1, 3},
 	}
 	return initCharacter(n, c, 0, pvMax, pvMax/2, []string{"Coup de poing"}, inventaire, 100)
 }
